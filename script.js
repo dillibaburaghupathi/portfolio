f0c7481a4ae119ea281864206f0bdafd3376959e
dillibaburaghupathi <dillibaburaghupathi@gmail.com>
2026-01-06 19:03:31 +0530

    Final Commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navLinks.classList.toggle("active");
+});
+
+// Close menu when a link is clicked
+document.querySelectorAll(".nav-links li a").forEach(n => n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navLinks.classList.remove("active");
+}));
